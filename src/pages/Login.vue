f5ed0e63926e19784fa92dec7a79dbc44a6f41d4
015697172275 <moacir.viana@tre-am.jus.br>
2026-03-18 06:51:28 -0400

    funcao wait

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,6 +9,7 @@
               v-model="username"
               label="Usuário"
               prepend-inner-icon="mdi-account"
+              :rules="[(v) => !!v || 'Campo obrigatório']"
               required
             />
             <v-text-field
@@ -16,12 +17,16 @@
               label="Senha"
               type="password"
               prepend-inner-icon="mdi-lock"
+              :rules="[(v) => !!v || 'Campo obrigatório']"
               required
             />
             <v-btn type="submit" color="primary" block :loading="loading"
               >Entrar</v-btn
             >
           </v-form>
+          <v-alert v-if="error" type="error" variant="tonal" class="mt-4">
+            {{ error }}
+          </v-alert>
         </v-card-text>
       </v-card>
     </v-col>
@@ -29,10 +34,11 @@
 </template>
 
 <script setup lang="ts">
+import { wait } from "@/services/Utils";
 import { ref } from "vue";
 //import { useAuthStore } from '@/stores/auth'
 //import { useRouter } from 'vue-router'
-
+const error = ref("");
 //const authStore = useAuthStore()
 //const router = useRouter()
 const username = ref("");
@@ -42,13 +48,15 @@ const loading = ref(false);
 const handleLogin = async () => {
   loading.value = true;
   //const success = await authStore.login(username.value, password.value)
-  loading.value = false;
+  await wait(2500);
+
   if (true) {
     //router.push('/clientes')
-    alert("Credenciais inválidas");
+    error.value = "Usuário ou senha inválidos";
   } else {
-    alert("Credenciais inválidas");
+    error.value = "Usuário ou senha inválidos";
   }
+  loading.value = false;
 };
 </script>
 
